3b0b4503e2fa2588f4ad395e7a2bdd58e7c66d23
“[Tom <[thomas.j.a.graham@gmail.com]>
2021-05-26 15:50:09 -0400

    updated analysis

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from rdkit.Chem import PandasTools
+from analysis import query_fp_search
 
 import molbeam
 import similarity
 
 
-def process_batch(query_names, query_matrix, mol_batch, threshold=.75):
+def process_batch(query_names, query_matrix, mol_batch, threshold=.99):
     fingerprints = mol_batch.column('achiral_fp')
     fp_matrix_in = similarity.build_fingerprint_matrix(fingerprints)
     fp_distance = similarity.fast_jaccard(fp_matrix_in, query_matrix)
@@ -17,15 +17,17 @@ def process_batch(query_names, query_matrix, mol_batch, threshold=.75):
         result = pd.DataFrame(fp_distance, columns=['PBB3', 'PM_PBB3', 'C5_05'])
         result.insert(0, 'canonical_id', mol_batch.column('canonical_ID'))
         result.insert(1, 'std_smiles', mol_batch.column('enumerated_smiles'))
+
+
     return result
 
 
 def format_results(result_list, query_names):
     result_df = pd.concat(result_list).sort_values(by=query_names)
 
-    PandasTools.AddMoleculeColumnToFrame(result_df, smilesCol='std_smiles', molCol='self')
+    # PandasTools.AddMoleculeColumnToFrame(result_df, smilesCol='std_smiles', molCol='self')
     # PandasTools.AddMoleculeColumnToFrame(combined_df, smilesCol='nn_smiles', molCol='near_neighbor')
-    PandasTools.RenderImagesInAllDataFrames()
+    # PandasTools.RenderImagesInAllDataFrames()
     return result_df
 
 
@@ -47,9 +49,15 @@ def main():
         result = process_batch(query_names, query_matrix, mol_batch, threshold)
         if result is not None:
             results.append(result)
+        
 
+    distance = 0.75
     final_df = format_results(results, query_names)
-    print(final_df.head(20))
+    # print(len(final_df))
+    # print(final_df.head(20))
+
+    output = query_fp_search(final_df, distance)
+    print(output)
 
 
 if __name__ == '__main__':
