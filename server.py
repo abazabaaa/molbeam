710e59ada456a0233a2a1a683b1824fbdcb8ea2a
Chadwicke Cooper <coop@iris.me>
2021-06-06 10:14:56 -0500

    More efficient schema fetch for flight server

--- server.py
+++ server.py
@@ -8,18 +8,23 @@ class FlightServer(fl.FlightServerBase):
     def __init__(self, location="grpc://0.0.0.0:8815", **kwargs):
         super(FlightServer, self).__init__(location, **kwargs)
 
-        sample_data = ds.dataset("./sample_data", format="parquet")
+        # sample_data = ds.dataset("./sample_data", format="parquet")
+        sample_data = ds.dataset("s3://molbeam/tested", format="parquet")
         self.datasets = {
             b'molbeam': sample_data,
         }
 
     def do_get(self, context, ticket):
         dataset = self.datasets[ticket.ticket]
-        batches = dataset.to_batches(columns=["canonical_ID", "enumerated_smiles", "achiral_fp"])
-        b = dataset.to_batches(columns=["canonical_ID", "enumerated_smiles", "achiral_fp"])
-        s = list(b)[0].schema
 
-        return fl.GeneratorStream(s, batches)
+        # Duplicated generator just to get the schema :(
+        b = dataset.to_batches(columns=["canonical_ID", "enumerated_smiles", "achiral_fp"])
+        head = next(b)
+        schema = head.schema
+        print('schema done')
+
+        batches = dataset.to_batches(columns=["canonical_ID", "enumerated_smiles", "achiral_fp"])
+        return fl.GeneratorStream(schema, batches)
 
 
 def main():
